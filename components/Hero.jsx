3c7daf848aa8aa4d00ad93297f5ae4acc9da6c5f
zenode <46578099+zenodegenkamp@users.noreply.github.com>
2023-07-10 11:25:22 +0200

    Add files via upload

---
+++ components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+export default function Hero(){
+    return (
+        <div className="hero">
+            <div className="hero-container">
+                <h1 className="title-text">My adventure in Thailand</h1>
+                <p className="small-title-text">"Embark backpacking adventure through the captivating landscapes and vibrant culture of Thailand." </p>
+            </div>
+            <div className="positionRight">
+                <h1>Top 5 locations</h1>
+            </div>
+            <div className="profile-container">
+                    <img className ="profile-pic" src="./images/profile-picture.jpeg" />
+                    <p className="small-text">Zeno Degenkamp</p>
+            </div>
+        </div>
+    )
+}
+
+
